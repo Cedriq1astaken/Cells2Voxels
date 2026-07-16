9e2e55a780780ffe49922f9442ad954602aa1cae
cedri <desinorcedric3@gmail.com>
2026-07-16 01:04:32 -0400

    Remove legacy demo files

--- src/loadShader.js
+++
@@ -1,9 +0,0 @@
-const loadShader = (path) =>
-  fetch(new URL(path, import.meta.url)).then((response) => {
-    if (!response.ok) {
-      throw new Error(`Couldn't load shader: ${path}`);
-    }
-    return response.text();
-  });
-
-export default loadShader;
